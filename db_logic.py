c58ea4e8b2c12cd19c68a1ba557621cff48e512d
f <lol@gmail.com>
2022-09-04 22:55:09 +0300

    еще мнооого надо доделать)

---
+++ db_logic.py
@@ -0,0 +1,66 @@
+import sqlite3
+
+
+def get_connection(filepath):
+    try:
+        with open(filepath, 'x') as fp:
+            pass
+    except:
+        pass
+
+    conn = sqlite3.connect(filepath)
+    cur = conn.cursor()
+    return conn, cur
+
+
+def check_table(conn, cur):
+    query = f"""
+    CREATE TABLE IF NOT EXISTS users_info(
+        login       TEXT PRIMARY KEY,
+        password    TEXT
+    );
+    """
+    cur.execute(query)
+    conn.commit()
+
+
+def count_users_with_such_login(login):
+    conn, cur = get_connection('db.db')
+
+    check_table(conn, cur)
+
+    query = f"""
+    SELECT COUNT(*) FROM users_info
+    WHERE login = "{login}"
+    """
+    cur.execute(query)
+    number_of_users_with_such_login = cur.fetchall()[0][0]
+
+    return number_of_users_with_such_login
+
+
+def add_user(login, password):
+    conn, cur = get_connection('db.db')
+
+    check_table(conn, cur)
+
+    query = f"""
+    INSERT INTO users_info (login, password)
+    VALUES ("{login}", "{password}")
+    """
+    cur.execute(query)
+    conn.commit()
+
+
+def get_password_by_login(login):
+    conn, cur = get_connection('db.db')
+    check_table(conn, cur)
+
+    query = f"""
+    SELECT password FROM users_info
+    WHERE login = "{login}"
+    """
+    cur.execute(query)
+    the_password = cur.fetchall()[0][0]
+
+    return the_password
---
+++ flask_server.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, make_response, session
+from time import asctime
+import db_logic
+import os
+
+server_object = Flask(__name__)
+
+
+@server_object.route('/', methods=['GET', 'POST'])
+def index():
+    # чтобы получить тип запроса из функции, обрабатывающей запрос,
+    # вызываем request.method
+    if 'time' in session:
+        print('Yes')
+    else:
+        print('No')
+    session['time'] = asctime()
+    if request.method == 'GET':
+        if 'time' in session:
+            return render_template('index.html', time=session['time'])
+        return render_template('index.html')
+
+
+@server_object.route('/page2.html', methods=['GET', 'POST'])
+def give_page2():
+    return '<h1>page2</h1>'
+
+
+@server_object.route('/show_template.html', methods=['GET', 'POST'])
+def show_template():
+    if request.method == 'GET':
+        vremya_stroka = asctime()
+        return render_template('show_template.html', time_param=vremya_stroka)
+
+
+@server_object.route('/login.html', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    user_login, user_password = request.form['login'], request.form['password']
+
+    such_users = db_logic.count_users_with_such_login(user_login)
+    if such_users != 0:
+        true_password = db_logic.get_password_by_login(user_login)
+
+        if true_password == user_password:
+            session['is_logged_in'] = True
+            session['login'] = user_login 
+            return render_template('index.html')
+        return render_template('login.html', wrong_password=True)
+
+    return render_template('login.html', wrong_login=True)
+
+
+@server_object.route('/registration.html', methods=['GET', 'POST'])
+def registration():
+    if request.method == 'GET':
+        return render_template('registration.html')
+
+    new_login, new_password = request.form['login'], request.form['password']
+
+    such_users = db_logic.count_users_with_such_login(new_login)
+    if such_users == 0:
+        db_logic.add_user(new_login, new_password)
+        session['is_logged_in'] = True
+        session['login'] = user_login 
+        return render_template('index.html')
+
+    true_password = db_logic.get_password_by_login(new_login)
+    if true_password == new_password:
+        session['is_logged_in'] = True
+        session['login'] = user_login 
+        return render_template('index.html')
+
+    return render_template('registration.html', wrong_password=True)
+
+
+@server_object.route('/lecture_<lecture_num>.html', methods=['GET', 'POST'])
+def lecture(lecture_num):
+    return render_template(f'/lectures/lecture_{lecture_num}.html', lecture_num=int(lecture_num), lectures_total_num = len(os.listdir('templates/lectures')))
+
+
+@server_object.route('/lectures_table_of_contents.html', methods=['GET', 'POST'])
+def lectures_table_of_contents():
+    return render_template('/lectures_table_of_contents.html', lectures_list = os.listdir('templates/lectures'))
+
+
+
+if __name__ == '__main__':
+    server_object.secret_key = 'abodwdwfebtgta'
+    server_object.run(debug=True)
