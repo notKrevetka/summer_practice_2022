523e2566e883f4a19406c4c03a2ff94c287638ff
krevetka <sasha.n2230@gmail.com>
2022-09-11 18:20:56 +0300

    vse ok vrode

--- flask_server.py
+++ flask_server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, make_response, session
 import db_logic
 import os
 import json
+import re
 
 server_object = Flask(__name__)
 
@@ -79,17 +80,26 @@ def lecture(lecture_num):
 
 @server_object.route('/content_lectures.html', methods=['GET', 'POST'])
 def lectures_table_of_contents():
-    return render_template('/content_lectures.html', lectures_list=os.listdir('templates/lectures'))
+    lectures_list = os.listdir('templates/lectures')
+    titles = []
+    for name in lectures_list:
+        with open(f'templates/lectures/{name}', 'r') as f:
+            text = f.read()
+        title = re.search(r'<title>(.*)</title>', text).groups()[0]
+        titles.append(title)
+    return render_template('/content_lectures.html', lectures_list=zip(lectures_list, titles))
 
 
 @server_object.route('/test_<test_num>.html', methods=['GET', 'POST'])
 def test(test_num):
-    with open('test_content.json', 'r', encoding='utf-8') as f:
-        all_test_obj = json.load(f)
-        all_tests_list = all_test_obj["tests"]
-        this_test = all_tests_list[int(test_num)-1]
-        print(this_test, test_num)
-    return render_template('/test_base.html', test_num=int(test_num), this_test=this_test)
+    if request.method == 'GET':
+        with open('test_content.json', 'r', encoding='utf-8') as f:
+            all_test_obj = json.load(f)
+            all_tests_list = all_test_obj["tests"]
+            this_test = all_tests_list[int(test_num)-1]
+            print(this_test, test_num)
+        return render_template('/test_base.html', test_num=int(test_num), this_test=this_test)
+        
 
 
 @server_object.route('/content_tests.html', methods=['GET', 'POST'])
